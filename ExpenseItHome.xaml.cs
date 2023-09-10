d21a2c26db137073e48f324dd865fb0854f013b4
shiyao <138352779+Chuya111@users.noreply.github.com>
2023-09-10 23:37:10 +0800

    Add files via upload

---
+++ ExpenseItHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)//事件响应者是ExpenseItHome ，事件处理器是Button_Click，
+                                                                   //只不过在这个方法里面用到了expenseReportPage
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
